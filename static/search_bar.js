9519b6232cfed4b7df237293f0da581f66d6856f
AINS <59402342+ainssssss@users.noreply.github.com>
2023-11-06 12:01:03 +0100

    Update search_bar.js

--- static/search_bar.js
+++ static/search_bar.js
@@ -1,4 +1,7 @@
 function redirigir() {
     var texto = document.getElementById("search-character-input").value;
+    if (texto.trim() === "") {
+        texto = "none";
+    }
     window.location.href = "/search/q=" + texto;
 }
